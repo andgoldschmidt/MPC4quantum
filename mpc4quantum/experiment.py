373361739db4526b921e98fc5a0b00e31e937076
andy <agoldschmidt11@gmail.com>
2021-07-20 14:58:47 -0700

    Add model, experiment classes.

---
+++ mpc4quantum/experiment.py
@@ -0,0 +1,276 @@
+from abc import ABC, abstractmethod
+import numpy as np
+from qutip import mesolve, propagator, Qobj
+from scipy.integrate import solve_ivp
+from scipy.interpolate import interp1d
+
+
+class Experiment(ABC):
+    """
+    The Experiment class is an interface for simulating ODEs with external controls.
+    """
+    def __init__(self):
+        # Save the last simulation
+        self.ts = None
+        self.us = None
+        self.xs = None
+
+    @abstractmethod
+    def f(self, t, x, u):
+        """
+        The function evaluation of the ODE.
+
+        :param t: Current time
+        :param x: Current state
+        :param u: Current control
+        :return: The time derivative of the state.
+        """
+
+    @staticmethod
+    def lift(x):
+        return x
+
+    @staticmethod
+    def proj(z):
+        return z
+
+    @abstractmethod
+    def simulate(self, x0, ts, us):
+        """
+        Solve the initial value problem starting at x0 for the times spanning the first and last values of ts. The
+        output is evaluated at all times in ts.
+
+        :param x0: The initial state.
+        :param ts: The time series to evaluate the output states.
+        :param us: The control can be either a function of time or an ndarray of the length of ts.
+        :return: The solution state of the initial value problem.
+        """
+
+
+class CExperiment(Experiment):
+    """
+    The CExperiment class implements solve_ivp to solve classical experiments.
+    """
+    def __init__(self):
+        # Save the last simulation
+        super().__init__()
+        self._interpolation = 'linear'
+
+    @abstractmethod
+    def f(self, t, x, u):
+        """
+        The function evaluation of the ODE.
+
+        :param t: Current time
+        :param x: Current state
+        :param u: Current control
+        :return: The time derivative of the state.
+        """
+
+    def _f(self, t, x):
+        return self.f(t, x, self.us(t)).flatten()
+
+    @staticmethod
+    def lift(x):
+        return x
+
+    @staticmethod
+    def proj(z):
+        return z
+
+    def simulate(self, x0, ts, us):
+        """
+        Solve the initial value problem starting at x0 for the times spanning the first and last values of ts. The
+        output is evaluated at all times in ts.
+
+        :param x0: The initial state.
+        :param ts: The time series to evaluate the output states.
+        :param us: The control can be either a function of time or an ndarray of the length of ts.
+        :return: The solution state of the initial value problem.
+        """
+        self.ts = ts
+        # Control function or series must map times to inputs
+        self.us = us if callable(us) else interp1d(ts, us, kind=self._interpolation)
+        res = solve_ivp(self._f, [self.ts[0], self.ts[-1]], x0, t_eval=self.ts)
+        self.xs = res.y
+        return res.y
+
+
+class VanDerPol(CExperiment):
+    """
+    To define a new kind of experiment, we need to specify the ODE function, f. This class simulates the Van der Pol
+    oscillator.
+
+    We can also use this instance to add system-specific characteristics. For example, in this class we defined a lift
+    function in order to standardize a Koopman linearization of the Van der Pol oscillator.
+
+    We also add LQR set/get methods for interactin with the LQR algorothm.
+    """
+    def __init__(self, mu):
+        super().__init__()
+        self.dim_x = 2
+        self.dim_u = 1
+        self.mu = mu
+
+        # Set/Get for LQR
+        self.Q = None
+        self.R = None
+        self.Qf = None
+        self.target = None
+        self._set_cost = False
+        self._set_target = False
+
+    def f(self, t, x, u):
+        x1, x2 = x
+        return np.block([
+            x2,
+            -x1 + self.mu * (1 - x1 ** 2) * x2 + u
+        ])
+
+    @staticmethod
+    def lift(x):
+        x1, x2 = x
+        return np.vstack([
+            x1, x2, x1 ** 2, x1 ** 2 * x2
+        ])
+
+    @staticmethod
+    def proj(z):
+        return z[:2, :]
+
+    def set_cost(self, Q, R, Qf):
+        self.Q = Q
+        self.R = R
+        self.Qf = Qf
+        self._set_cost = True
+
+    def get_cost(self):
+        if self._set_cost:
+            return self.Q, self.R, self.Qf
+        else:
+            raise RuntimeError("LQR cost not set.")
+
+    def set_target(self, target):
+        self.target = target
+        self._set_target = True
+
+    def get_target(self):
+        if self._set_target:
+            return self.Q, self.R, self.Qf
+        else:
+            raise RuntimeError("LQR target not set.")
+
+
+class Rotor(CExperiment):
+    """
+    A simple controlled rotation matrix.
+    """
+    def __init__(self, epsilon):
+        super().__init__()
+        self.dim_x = 2
+        self.dim_u = 1
+        self.epsilon = epsilon
+
+    def f(self, t, x, u):
+        x1, x2 = x
+        omega = 1 + self.epsilon * u
+        return np.block([
+            omega * x2,
+            -omega * x1
+        ])
+
+
+class QExperiment(Experiment):
+    """
+    The QExperiment class implements qutip's mesolve to solve quantum state preparation experiments.
+    """
+    def __init__(self, H0, H1_list):
+        super().__init__()
+        self.H0 = H0
+        self.H1_list = H1_list
+        self._me_args = {}
+
+    def f(self, t, x, u):
+        return self.H0 * x + np.sum([H1 * x * u1 for H1, u1 in zip(self.H1_list, u)], axis=0)
+
+    def set(self, key, value):
+        """
+        Set a keyword argument for QuTip's mesolve.
+        """
+        self._me_args[key] = value
+
+    @staticmethod
+    def _wrap_u(u_func, index_u):
+        def new_func(ts, args):
+            return u_func(ts)[index_u]
+        return new_func
+
+    def simulate(self, x0, ts, us):
+        self.set('rho0', Qobj(x0.reshape(*self.H0.shape)))
+        self.ts = ts
+        self.set('tlist', self.ts)
+        # Assume us is a 1dInterp object if callable.
+        if callable(us):
+            u_dim = us.y.reshape(-1, len(us.x)).shape[0]
+            self.us = np.array([self._wrap_u(us, i) for i in range(u_dim)])
+        else:
+            self.us = np.atleast_2d(us)
+            u_dim = self.us.shape[0]
+        self.set('H', [self.H0] + [[self.H1_list[i_row], self.us[i_row]] for i_row in range(u_dim)])
+        res = mesolve(**self._me_args)
+        self.xs = np.array(res.expect) if 'e_ops' in self._me_args else np.array(res.states).reshape(len(ts), -1).T
+        return self.xs
+
+
+class QSynthesis(Experiment):
+    """
+    The QSynthesis class implements qutip's propogator to solve quantum gate synthesis.
+    """
+    def __init__(self, H0, H1_list):
+        super().__init__()
+        self.H0 = H0
+        self.H1_list = H1_list
+        self._prop_args = {}
+
+    def f(self, t, x, u):
+        return self.H0 * x + np.sum([H1 * x * u1 for H1, u1 in zip(self.H1_list, u)], axis=0)
+
+    def set(self, key, value):
+        """
+        Set a keyword argument for QuTip's propagator.
+        """
+        self._prop_args[key] = value
+
+    @staticmethod
+    def _wrap_u(u_func, index_u):
+        def new_func(ts, args):
+            return u_func(ts)[index_u]
+        return new_func
+
+    def simulate(self, x0, ts, us):
+        # Shape the initial condition into a propagator
+        x0 = Qobj(x0.reshape(*self.H0.shape))
+        self.ts = ts
+        # Assume us is a 1dInterp object if callable.
+        if callable(us):
+            u_dim = us.y.reshape(-1, len(us.x)).shape[0]
+            self.us = np.array([self._wrap_u(us, i) for i in range(u_dim)])
+        else:
+            self.us = np.atleast_2d(us)
+            u_dim = self.us.shape[0]
+        self.set('H', [self.H0] + [[self.H1_list[i_row], self.us[i_row]] for i_row in range(u_dim)])
+        # Catch a qutip bug for ts <= 2.
+        # Unitary mode 'single' avoids the need to check if dtype is Qobj or memoryview
+        if len(self.ts) > 2:
+            self.set('t', self.ts)
+            self.xs = propagator(**self._prop_args, unitary_mode='single')
+        else:
+            self.xs = [None] * len(self.ts)
+            for i, t in enumerate(self.ts):
+                self.set('t', t)
+                self.xs[i] = propagator(**self._prop_args, unitary_mode='single')
+        # Append the initial condition to the resulting ndarrays via multiplication, U(t, t0) @ U(t0, 0)
+        if len(self.xs) > 0:
+            if self.xs[0]:
+                self.xs = np.vstack([(xi * x0).data.toarray().flatten() for xi in self.xs]).T
+        return self.xs
---
+++ mpc4quantum/model.py
@@ -0,0 +1,299 @@
+import numpy as np
+# TODO:
+#   1.  Decide if U=None is permissible.
+#   2.  Insert dimension / operator checks and warnings
+
+
+class DMDc:
+    """
+    DMDc interface (the interface acts as a minimal container for read-only DMDc models).
+    """
+    def __init__(self, dim_y, dim_x, dim_u, A0):
+        """
+        A DMDc linear control model. The read-only version of the class can predict future states using the linear
+        control model and return views on the linear control operators. The fully-implemented version of the class must
+        update the fit model based on single iterations of new data (streaming data).
+
+        :param dim_y: Output state dimension.
+        :param dim_x: Input state dimension.
+        :param dim_u: Input control dimension.
+        :param A0: Initial model of shape (dim_y, dim_x * dim_u).
+        """
+        self.dim_y = dim_y
+        self.dim_x = dim_x
+        self.dim_u = dim_u
+        self.A = A0
+
+        # Default discount factor; if the half-life is k iterations, then the discount is 2^(-1/k).
+        self.discount = 1
+
+        # Default rcond for pinv regularization.
+        self.rcond = 1e-15
+
+    @classmethod
+    def from_data(cls, Y, X, U, **kwargs):
+        """
+        Fit a DMDc model using snapshots matrices.
+
+        :param Y: Output state data of shape (dim_y, n_snapshots).
+        :param X: Input state data of shape (dim_x, n_snapshots)
+        :param U: Input control data of shape (dim_u, n_snapshots)
+        :return: A DMDc object.
+        """
+        raise NotImplementedError()
+
+    @classmethod
+    def from_bootstrap(cls, dim_y, dim_x, dim_u, A0, **kwargs):
+        """
+        Fit a DMDc model by bootstrapping to an initial model.
+
+        :param dim_y: Output state dimension.
+        :param dim_x: Input state dimension.
+        :param dim_u: Input control dimension.
+        :param A0: Initial model of shape (dim_y, dim_x * dim_u).
+        :return: A DMDc object.
+        """
+        raise NotImplementedError()
+
+    @classmethod
+    def from_randn(cls, dim_y, dim_x, dim_u, **kwargs):
+        """
+        Fit a DMDc model by bootstrapping to a random model.
+
+        :param dim_y: Output state dimension.
+        :param dim_x: Input state dimension.
+        :param dim_u: Input control dimension.
+        :return: A DMDc object.
+        """
+        raise NotImplementedError()
+
+    def fit_iteration(self, next_y, next_x, next_u):
+        """
+        Streaming update of the DMDc model based on the next data.
+
+        :param next_y: Next ouput state.
+        :param next_x: Next input state.
+        :param next_u: Next control state.
+        :return: The updated linear control model as returned by self.get_discrete().
+        """
+        raise NotImplementedError()
+
+    def predict(self, current_x, current_u):
+        """
+        Make a prediction of the output data using the current model and the provided input data. Can predict from
+        single iterations or full snapshot matrices.
+
+        :param current_x: The input state of shape dim_x or (dim_x, n_snapshots)
+        :param current_u: The input control of shape dim_u or (dim_u, n_snapshots)
+        :return: The predicted output state of shape (dim_y, 1) or (dim_u, n_snapshots)
+        """
+        A_x, A_u = self.get_discrete()
+        current_x = current_x.reshape(self.dim_x, -1)
+        current_u = current_u.reshape(self.dim_u, -1)
+        return A_x @ current_x + A_u @ current_u
+
+    def get_discrete(self):
+        """
+        Return the linear control model A, B such that y = A @ x + B @ u.
+
+        :return: Two operators of shapes (dim_y, dim_x) and (dim_y, dim_u), respectively.
+        """
+        A_x = self.A[:self.dim_y, :self.dim_x]
+        A_u = self.A[:self.dim_y, self.dim_x:]
+        return A_x, A_u
+
+    # def get_continuous_from_discrete(self):
+    #     raise NotImplementedError()
+
+
+class DiscrepDMDc(DMDc):
+    """
+    Offline DMDc implementation. Uses discprepancies to enable bootstrapping.
+
+    Notes:
+        1.  Consider improvements with streaming ideas to avoid repeat pinv.
+    """
+    def __init__(self, dim_y, dim_x, dim_u, A0, **kwargs):
+        super().__init__(dim_y, dim_x, dim_u, A0)
+        self.initialization = kwargs
+
+        # Update available kwargs
+        self.Y = kwargs['Y'] if 'Y' in kwargs else None
+        self.X = kwargs['X'] if 'X' in kwargs else None
+        self.U = kwargs['U'] if 'U' in kwargs else None
+        self.discount = kwargs['discount'] if 'discount' in kwargs else self.discount
+        self.rcond = kwargs['rcond'] if 'rcond' in kwargs else self.rcond
+
+        # Saved data
+        self.iA = [A0]
+        self._save = False
+        self._iteration = 0
+        self._isave = 10
+
+    @classmethod
+    def from_randn(cls, dim_y, dim_x, dim_u, **kwargs):
+        """
+        Fit a DMDc model by bootstrapping to a (real) random normal model of a specified standard deviation.
+
+        :param dim_y: Output state dimension.
+        :param dim_x: Input state dimension.
+        :param dim_u: Input control dimension.
+        :param kwargs: Requires sigma, the standard deviation of the normal distribution for the random operator.
+        :return: A DMDc object.
+        """
+        sigma = kwargs['sigma']
+        A0 = np.random.randn(dim_y, dim_x + dim_u) * sigma
+        return cls(dim_y, dim_x, dim_u, A0, **{'sigma': sigma})
+
+    @classmethod
+    def from_bootstrap(cls, dim_y, dim_x, dim_u, A0, **kwargs):
+        # docstring inherited
+        return cls(dim_y, dim_x, dim_u, A0)
+
+    @classmethod
+    def from_data(cls, Y, X, U=None, **kwargs):
+        """
+        Fit a DMDc model using snapshots matrices.
+
+        :param Y: Output state data of shape (dim_y, n_snapshots).
+        :param X: Input state data of shape (dim_x, n_snapshots)
+        :param U: Input control data of shape (dim_u, n_snapshots)
+        :param kwargs: Requires rcond, the rcond to use in pinv for the fit.
+        :return: A DMDc object.
+        """
+        rcond = kwargs['rcond']
+        dim_y = Y.shape[0]
+        dim_x = X.shape[0]
+        if U is None:
+            dim_u = 0
+            Z = X
+        else:
+            dim_u = U.shape[0]
+            Z = np.vstack([X, U])
+        # Singular values less than or equal to rcond * largest_singular_value are set to zero
+        A0 = Y @ np.linalg.pinv(Z, rcond=rcond)
+        return cls(dim_y, dim_x, dim_u, A0, **{'Y': Y, 'X': X, 'U': U, 'rcond': rcond})
+
+    @staticmethod
+    def _update_stack(val, stack, discount):
+        val = val.reshape(-1, 1)
+        return val if stack is None else np.hstack([discount * stack, val])
+
+    def fit_iteration(self, next_y, next_x, next_u=np.array([])):
+        # docstring inherited
+        # Update data matrices
+        self.Y = self._update_stack(next_y, self.Y, self.discount)
+        self.X = self._update_stack(next_x, self.X, self.discount)
+        self.U = self._update_stack(next_u, self.U, self.discount)
+
+        # Add the current discrepancy model to the previous model.
+        current_Y = self.predict(self.X, self.U)
+        current_Z = np.vstack([self.X, self.U])
+        A1 = (self.Y - current_Y) @ np.linalg.pinv(current_Z, rcond=self.rcond)
+        self.A = self.A + A1
+        self._iteration += 1
+        if self._save and self._iteration % self._isave == 0:
+            self.iA.append(np.copy(self.A))
+
+        # Return the current linear model
+        return self.get_discrete()
+
+
+class OnlineDMDc(DMDc):
+    """
+    Online DMDc for overconstrained systems.
+    C.f. Online Dynamic Mode Decomposition for Time-Varying Systems by Zhang et al.
+
+    Notes:
+        1.  Consider taking the SVD of the data matrix X in order to include a rank truncation for noisy data. Can
+            this be applied iteratively?
+        2.  Robust regularization of the recursive least squares algorithm? Or, how to regularize the rank-1 updates?
+    """
+
+    def __init__(self, dim_y, dim_x, dim_u, P0, A0, **kwargs):
+        super().__init__(dim_y, dim_x, dim_u, A0)
+        self.initialization = kwargs
+
+        # Store P
+        self.P = P0
+
+        # Saved data
+        self.iP = [P0]
+        self.iA = [A0]
+        self._save = False
+        self._iteration = 0
+        self._isave = 10
+
+    @classmethod
+    def from_randn(cls, dim_y, dim_x, dim_u, **kwargs):
+        """
+        Fit a DMDc model by bootstrapping to a (real) random normal model of a specified standard deviation.
+
+        :param dim_y: Output state dimension.
+        :param dim_x: Input state dimension.
+        :param dim_u: Input control dimension.
+        :param kwargs:
+            Requires:
+                sigma, the standard deviation of the normal distribution for the random operator.
+                alpha, the update rate multiplying the estimated P operator (unknown covariance) (try: 1e2)
+        :return: A DMDc object.
+        """
+        sigma = kwargs['sigma']
+        alpha = kwargs['alpha']
+        dim_z = dim_x + dim_u
+        P0 = alpha * np.identity(dim_z)
+        A0 = np.random.randn(dim_y, dim_z) * sigma
+        return cls(dim_y, dim_x, dim_u, P0, A0, **kwargs)
+
+    @classmethod
+    def from_bootstrap(cls, dim_y, dim_x, dim_u, A0, **kwargs):
+        """
+        Fit a DMDc model by bootstrapping to an initial model.
+
+        :param dim_y: Output state dimension.
+        :param dim_x: Input state dimension.
+        :param dim_u: Input control dimension.
+        :param A0: Initial model of shape (dim_y, dim_x * dim_u).
+        :param kwargs:
+            Requires alpha, the update rate multiplying the estimated P operator (unknown covariance) (try: 1e2)
+        :return: A DMDc object.
+        """
+        alpha = kwargs['alpha']
+        P0 = alpha * np.identity(dim_x + dim_u)
+        return cls(dim_y, dim_x, dim_u, P0, A0, **kwargs)
+
+    @classmethod
+    def from_data(cls, Y, X, U=None, **kwargs):
+        # docstring inherited
+        dim_y = Y.shape[0]
+        dim_x = X.shape[0]
+        if U is None:
+            dim_u = 0
+            Z = X
+        else:
+            dim_u = U.shape[0]
+            Z = np.vstack([X, U])
+        # We assume this is full rank and not ill-conditioned.
+        P0 = np.linalg.pinv(Z @ Z.T)
+        A0 = Y @ Z.T @ P0
+        return cls(dim_y, dim_x, dim_u, P0, A0, **{'Y': Y, 'X': X, 'U': U})
+
+    def fit_iteration(self, next_y, next_x, next_u=np.array([])):
+        # docstring inherited
+        # Compute state and output with useful operator products
+        next_y = next_y.reshape(-1, 1)
+        next_z = np.vstack([next_x.reshape(-1, 1), next_u.reshape(-1, 1)])
+        Az = self.A @ next_z
+        Pz = self.P @ next_z
+        gamma = 1 / (1 + next_z.T @ Pz)
+
+        # Online update
+        self.A = self.A + gamma * (next_y - Az) @ Pz.T
+        self.P = (self.P - gamma * Pz @ Pz.T) / self.discount
+        self._iteration += 1
+        if self._save and self._iteration % self._isave == 0:
+            self.iA.append(np.copy(self.A))
+            self.iP.append(np.copy(self.P))
+
+        # Return the current linear model
+        return self.get_discrete()
